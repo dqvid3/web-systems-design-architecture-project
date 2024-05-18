697e6327b696045a10244b351a1a0a739f658f58
dqvid3 <davidebonura52@gmail.com>
2024-05-18 17:23:41 +0200

    prova

---
+++ JakartaEE/src/main/java/it/unipa/wsda/jakartaee/DBConnection.java
@@ -0,0 +1,59 @@
+package it.unipa.wsda.jakartaee;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import javax.sql.DataSource;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        Context ctx;
+        DataSource ds;
+        Connection connection;
+        try {
+            ctx = new InitialContext();
+            ds = (DataSource) ctx.lookup("java:comp/env/jdbc/monitoraggio");
+            connection = ds.getConnection();
+        }
+        catch (NamingException | SQLException exc) {
+            System.err.println("Errore durante la connessione: " + exc.getMessage());
+            connection = null;
+        }
+        return connection;
+    }
+
+    public static List<Impianto> eseguiQuery(String query) {
+        List<Impianto> impianti = null;
+        try (Connection connection = DBConnection.getConnection()) {
+            try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    impianti = new LinkedList<>();
+                    while (rs.next()) {
+                        // Package org.springframework.jdbc.core
+                        Impianto impianto = new Impianto(rs.getInt("cod_impianto"), rs.getString("descrizione"),
+                            rs.getBigDecimal("latitudine"), rs.getBigDecimal("longitudine"), rs.getTimestamp("ultimo_segnale"));
+                        impianti.add(impianto);
+                    }
+                }
+            }
+        }
+        catch (SQLException eccezione) { // Si potrebbe mettere un catch per ogni try in modo da differenziare i messaggi di errore...
+            System.err.println("Errore durante l'esecuzione della query: " + eccezione.getMessage());
+        }
+        return impianti;
+    }
+
+    public static int eseguiUpdate(String query, List<String> parametri) {
+        return 0;
+    }
+}
---
+++ JakartaEE/src/main/java/it/unipa/wsda/jakartaee/Impianto.java
@@ -0,0 +1,77 @@
+package it.unipa.wsda.jakartaee;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class Impianto {
+    private int cod_impianto;
+    private String descrizione;
+    private BigDecimal latitudine;
+    private BigDecimal longitudine;
+    private Timestamp ultimo_segnale;
+
+    public Impianto(int cod_impianto, String descrizione, BigDecimal latitudine, BigDecimal longitudine, Timestamp ultimo_segnale) {
+        this.setCod_impianto(cod_impianto);
+        this.setDescrizione(descrizione);
+        this.setLatitudine(latitudine);
+        this.setLongitudine(longitudine);
+        this.setUltimo_segnale(ultimo_segnale);
+    }
+
+    public int getCod_impianto() {
+        return this.cod_impianto;
+    }
+
+    public String getDescrizione() {
+        return this.descrizione;
+    }
+
+    public BigDecimal getLatitudine() {
+        return this.latitudine;
+    }
+
+    public BigDecimal getLongitudine() {
+        return this.longitudine;
+    }
+
+    public Timestamp getUltimo_segnale() {
+        return this.ultimo_segnale;
+    }
+
+    public void setCod_impianto(int cod_impianto) {
+        this.cod_impianto = cod_impianto;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setLatitudine(BigDecimal latitudine) {
+        this.latitudine = latitudine;
+    }
+
+    public void setLongitudine(BigDecimal longitudine) {
+        this.longitudine = longitudine;
+    }
+
+    public void setUltimo_segnale(Timestamp ultimo_segnale) {
+        this.ultimo_segnale = ultimo_segnale;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder impianto = new StringBuilder();
+        impianto.append(this.getLatitudine()).append("\t").append(this.getLongitudine()).append("\tImpianto ")
+            .append(this.getCod_impianto()).append("\t").append(this.getDescrizione()).append("<br><br><B>");
+        impianto.append(this.isAttivo() ? "Attivo" : "Non attivo");
+        String ultimo_segnale = this.getUltimo_segnale().toString().substring(0, this.getUltimo_segnale().toString().indexOf("."));
+        impianto.append("</B><br>Ultimo segnale: <B>").append(ultimo_segnale).append("</B>\t");
+        impianto.append(this.isAttivo() ? "green.png" : "red.png").append("\t58,96\t").append("-29,-96\n");
+        return impianto.toString();
+    }
+
+    public boolean isAttivo() {
+        return (this.getUltimo_segnale().compareTo(new Timestamp(System.currentTimeMillis() -
+            java.time.Duration.ofMinutes(2).toMillis())) >= 0);
+    }
+}
