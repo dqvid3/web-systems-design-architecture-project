5a33acf8b15b64ac935d7f6435e148d6a487f0cd
dqvid3 <davidebonura52@gmail.com>
2024-05-18 19:46:52 +0200

    piccoli fix

---
+++ gestione/src/main/java/it/unipa/wsda/gestione/CartelloniApplication.java
@@ -0,0 +1,13 @@
+package it.unipa.wsda.gestione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartelloniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartelloniApplication.class, args);
+    }
+
+}
---
+++ gestione/src/test/java/it/unipa/wsda/gestione/CartelloniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipa.wsda.gestione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartelloniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
